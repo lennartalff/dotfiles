3b163629e03de29ec1ab1d1187e73887e939bc77
Thies Lennart Alff <lennart.alff@gmail.com>
2023-08-16 15:20:24 +0200

    merged update on lua config

--- .config/nvim/lua/lennartalff/set.lua
+++ .config/nvim/lua/lennartalff/set.lua
@@ -14,6 +14,14 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+vim.api.nvim_create_autocmd("FileTYpe", {
+    pattern = "cpp",
+    callback = function()
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.tabstop = 2
+    end
+})
+
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = 'yes'
 vim.opt.updatetime = 500

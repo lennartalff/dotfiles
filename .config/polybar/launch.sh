c0d17922f9f9ec276e6ae60ecf667ff803925f4d
Thies Lennart Alff <lennart.alff@gmail.com>
2023-09-25 09:55:37 +0200

    show polybar on all displays

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,4 +4,12 @@
 polybar-msg cmd quit
 
 echo "---" | tee -a /tmp/polybar1.log 
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload bar1 &
+    done
+else
+    polybar --reload bar1 &
+fi
+
+# polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
